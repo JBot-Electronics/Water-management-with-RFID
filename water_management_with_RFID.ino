f68dcf3574f0377f0f79389094d4493e728d7902
jovyinny <jovinemutelani@gmail.com>
2022-11-30 17:13:29 +0300

    made minor changes

--- water_management_with_RFID.ino
+++ water_management_with_RFID.ino
@@ -17,20 +17,27 @@
 #define TRIGGER_PIN A2   //trigger pin of ultrasonic sensor
 #define ECHO_PIN A3      //echo pin of ultrasonic sensor
 #define RELAY_PIN 8     // Pin where pump relay is connected
+
 // all in centimeters
 #define MAX_DISTANCE 95 // this is the max distance, the sensor should detect of which is equivalent to cylinder height
 #define DIAMETER 53
 
+
+/*============================
+ ************ Global variables*****************
+ * ============================
+ */
 const int numRows = 2, numCols = 16; //LCD variables cols and rowa
 int selected_block = 1, CustomerBlock[2] = {1, 2};
 
-byte AdminBlock[16] = {"Admin"};  byte Customer[2][16] = {{"Customer"}, {"00000"}};
+byte AdminBlock[16] = {"Admin"},Customer[2][16] = {{"Customer"}, {"00000"}};
 bool Relay_state = true;
+float current_volume;
 
 //This array is used for reading out a block.
 byte data_read[18], readbackblock1[18];
 
-int addr = 0;       //Address for eeprom
+int price_address = 0, pass_address = 1;    //Address for eeprom
 
 //KEYPAD
 const byte ROWS = 4;
@@ -41,19 +48,27 @@ char hexaKeys[ROWS][COLS] = {
   {'7', '8', '9'},
   {'*', '0', '#'}
 };
-byte rowPins[ROWS] = {5, 4, 3, 2}, colPins[COLS] = {6, 7, 8};
-
-int amount;
-float current_volume;
-String tag_cash;
+byte rowPins[ROWS] = {5, 6, 7, 8}, colPins[COLS] = {4, 3, 2};
 
 
-//Function declaration
+/*=========================
+ ************Function declaration*********
+ *=========================
+ */
+int readBlock(int blockNumber, byte arrayAddress[]);
+int writeBlock(int selected_block, byte arrayAddress[]);void options(); 
 void AdminMenu();
-void CustomerMenu(int);
-void UpdateScreen(int, int, String, bool);
+void CustomerMenu(int cash);
+void UpdateScreen(int col, int row, String message, bool clear_screen);
+float GetVolume();
+int read_keypad(bool Is_password);
+bool Authenticate();
 
-//Objects inistances
+
+/*========================
+ **********Objects inistances************
+ *========================
+ */
 LiquidCrystal_I2C Screen(0x27, numCols, numRows);
 NewPing WaterLevel(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 Keypad KeyBoard = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
@@ -62,8 +77,6 @@ MFRC522::MIFARE_Key key;
 
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
   SPI.begin();
   RFID.PCD_Init();
   Screen.init();
@@ -71,57 +84,58 @@ void setup() {
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
-
-    UpdateScreen(0, 0, "Water System",true);
-    delay(1000);
-    UpdateScreen(0, 1, "Scan the Tag",false);
-    delay(100);
-//    Screen.clear();
-
+  UpdateScreen(0, 0, "Water System", true);
+  delay(1000);
+  UpdateScreen(0, 1, "Scan the Tag", false);
+  delay(100);
+  Screen.clear();
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if ( ! RFID.PICC_IsNewCardPresent()) {
-    return;
-  }
-  if ( ! RFID.PICC_ReadCardSerial())
-  {
-    return;
-  }
-
+    if ( ! RFID.PICC_IsNewCardPresent()) {
+      return;
+    }
+    if ( ! RFID.PICC_ReadCardSerial())
+    {
+      return;
+    }
+    
   //  Read the first block to determine user's level
-  readBlock(selected_block, data_read);
-  String UserLevel = String((char*)data_read);
+    readBlock(selected_block, data_read);
+    String UserLevel = String((char*)data_read);
 
-
-  if (UserLevel == "Admin") {
-    AdminMenu();
-  }
-  else {
-    readBlock(CustomerBlock[1], readbackblock1);
-    int credit = String((char*)readbackblock1).toInt();
-    CustomerMenu(credit);
-  }
-
-
+    if (UserLevel == "Admin") {
+          bool is_authenticated=Authenticate();
+          if(is_authenticated){
+              AdminMenu();
+            }
+    }
+    else {
+      readBlock(CustomerBlock[1], readbackblock1);
+      int credit = String((char*)readbackblock1).toInt();
+      CustomerMenu(credit);
+    }
 }
 
 
-//custom functions
+/* ========================
+ * *********custom function definitions***
+ * ========================
+ */
 int readBlock(int blockNumber, byte arrayAddress[])
 {
   int largestModulo4Number = blockNumber / 4 * 4;
-  int trailerBlock = largestModulo4Number + 3; //determine trailer block for the sector
+  int trailerBlock = largestModulo4Number + 3; 
 
   //authentication of the desired block for access
   byte status = RFID.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(RFID.uid));
 
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print("PCD_Authenticate() failed (read): ");
-    Serial.println(RFID.GetStatusCodeName(status));
-    return 3;//return "3" as error message
-  }
+//  if (status != MFRC522::STATUS_OK) {
+//    Serial.print("PCD_Authenticate() failed (read): ");
+//    Serial.println(RFID.GetStatusCodeName(status));
+//    return 3;//return "3" as error message
+//  }
 
   byte buffersize = 18;
   status = RFID.MIFARE_Read(blockNumber, arrayAddress, &buffersize);
@@ -139,11 +153,11 @@ int writeBlock(int selected_block, byte arrayAddress[]) {
 
   //authentication of the desired block for access
   byte status = RFID.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(RFID.uid));
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print("Authentication failed: ");
-    Serial.println(RFID.GetStatusCodeName(status));
-    return 3;
-  }
+//  if (status != MFRC522::STATUS_OK) {
+//    Serial.print("Authentication failed: ");
+//    Serial.println(RFID.GetStatusCodeName(status));
+//    return 3;
+//  }
 
   //writing the block
   status = RFID.MIFARE_Write(selected_block, arrayAddress, 16);
@@ -156,25 +170,35 @@ int writeBlock(int selected_block, byte arrayAddress[]) {
 
 
 void options() {
-  UpdateScreen(0, 0, "ADMIN", true);
-  Screen.autoscroll();
-//    for (int thisChar = 0; thisChar < 10; thisChar++) {
-//    delay(500);
-//  }
-  UpdateScreen(0, 1, "[1]- Recharge Customer.. [2]-- Set one liter price", true);
-//  Screen.noAutoscroll();
+  Screen.setCursor(0, 0);
+  Screen.print("ADMIN");
+  String options[3] = {"[1]- Recharge Customer.. ", "[2]-- Set one liter price", " [3]-- Change Password"};
+
+  for (int j = 0; j < 3; j++) {
+    Screen.setCursor(0, 1);
+    Screen.print(options[j]);
+    delay(1000);
+    for (int PositionCount = 0; PositionCount < 20; PositionCount++)
+    {
+      Screen.scrollDisplayLeft();
+      delay(300);
+    }
+    Screen.clear();
+  }
 }
+
 
 void AdminMenu() {
   options();
-  char  key_pressed = KeyBoard.getKey();
-  UpdateScreen(0,0,String(key_pressed),false);
-
-  if (key_pressed == "4") {
-    UpdateScreen(0, 1, "Enter Amount: ", false);
-
-    read_keypad();
-    UpdateScreen(0, 1, "Scan Customer Card: ", false);
+  int amount;
+  char  key_pressed = read_keypad(false);
+  
+  if (key_pressed == "1") {
+    UpdateScreen(0, 0, "Enter Amount: ", false);
+    amount = read_keypad(false);
+    delay(100);
+    Screen.clear();
+    UpdateScreen(0, 0, "Place Customer Card: ", false);
     readBlock(CustomerBlock[1], readbackblock1);
     int cash = String((char*)readbackblock1).toInt();
     int updated_cash = cash + amount;
@@ -182,12 +206,11 @@ void AdminMenu() {
     // Update card
     writeBlock(CustomerBlock[1], updated_cash);
     UpdateScreen(0, 1, "Remove the tag", true);
-
   }
-  else if (key_pressed == "5") {
-    amount = read_keypad();
+  else if (key_pressed == "2") {
+    amount = read_keypad(false);
     UpdateScreen(0, 1, String(amount), true);
-    EEPROM.update(addr, amount);
+    EEPROM.update(price_address, amount);
   }
   else {
     options();
@@ -195,44 +218,12 @@ void AdminMenu() {
 }
 
 
-float GetVolume() {
-  float volume = (22 / 7) * (DIAMETER * DIAMETER) * WaterLevel.ping_cm() / 4;     //PI*r2h
-  return volume;
-}
-
-int read_keypad() {
-  int cursor_position = 0;
-  String amount;
-  
-  while (true) {
-    char key = KeyBoard.getKey();
-    
-    if (key == "*") {
-//      delete the preceeding numbers
-      UpdateScreen(cursor_position,1,"",false);
-      cursor_position-=1;
-      
-    }
-    else if( key == "#"){
-//      confim
-      return amount.toInt();
-      }
-    else {
-        UpdateScreen(cursor_position, 1, String(key), false);
-          amount[cursor_position] = key;
-      cursor_position += 1;
-    }
-  }
-}
-
-
 void CustomerMenu(int cash) {
-  Screen.autoscroll();
-  UpdateScreen(0, 0, "Enter amount in ltrs", true);
-  
-  int ltrs = read_keypad();
-  
-  float current_price = EEPROM.read(addr);
+  UpdateScreen(0, 0, "Enter amount in ltrs:", false);
+
+  int ltrs = read_keypad(false);
+
+  float current_price = EEPROM.read(price_address);
 
   if (GetVolume() < ltrs) {
     UpdateScreen(0, 0, "No Enough water", true);
@@ -265,6 +256,41 @@ void CustomerMenu(int cash) {
   }
 }
 
+float GetVolume() {
+  float volume = (22 / 7) * (DIAMETER * DIAMETER) * WaterLevel.ping_cm() / 4;     //PI*r2h
+  return volume;
+}
+
+
+int read_keypad(bool Is_password = false) {
+  int cursor_position = 0;
+  String amount;
+  char key;
+
+  //get key presses till # is preassed
+  while (KeyBoard.getKey() != "#") {
+    if (key == "*") {
+      //      delete the preceeding numbers
+      UpdateScreen(cursor_position, 1, "", false);
+      cursor_position -= 1;
+    }
+    else {
+      if (Is_password) {
+        UpdateScreen(cursor_position, 1, String(key), false);
+        delay(10);
+        UpdateScreen(cursor_position, 1, "*", false);
+      } else {
+        UpdateScreen(cursor_position, 1, String(key), false);
+        amount[cursor_position] = key;
+      }
+      cursor_position += 1;
+    }
+  }
+  return amount.toInt();
+}
+
+
+
 void UpdateScreen(int col, int row, String message, bool clear_screen = true) {
   if (clear_screen) {
     Screen.clear();
@@ -273,5 +299,40 @@ void UpdateScreen(int col, int row, String message, bool clear_screen = true) {
   } else {
     Screen.setCursor(col, row);
     Screen.print(message);
+  }
+}
+
+bool Authenticate() {
+  UpdateScreen(0, 0, "Enter Password: ", false);
+  int pass = read_keypad(true), stored_pass = EEPROM.read(pass_address);
+  int trials = 3;
+  if (stored_pass) {
+    while (trials > 0) {
+      if (stored_pass == pass) {
+        return true;
+      } else {
+        UpdateScreen(0, 0, "Enter Password: ", false);
+        UpdateScreen(0, 1, char(trials-1)+"Remaining", false);
+        pass = read_keypad(true);
+      }
+      trials -= 1;
+    }
+    return false;
+  }
+  else {
+    UpdateScreen(0, 0, "Set Password: ", false);
+    int new_pass = read_keypad(true);
+    UpdateScreen(0, 0, "Enter Password again: ", false);
+    int new_pass_1 = read_keypad(true);
+    if(new_pass==new_pass_1){
+      EEPROM.update(pass_address,new_pass);
+      return true;
+    }else{
+    UpdateScreen(0, 0, "Wrong password:", false);
+    UpdateScreen(0, 1, "Password is set to: ", false);
+    UpdateScreen(0, 0, String(1234), false);
+    EEPROM.update(pass_address,1234);
+    return true;
+    }
   }
 }
